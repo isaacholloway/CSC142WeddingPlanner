b5008f245619fccffb8d92793d6b4ae15cc58271
Isaac <isaacholloway98@gmail.com>
2018-11-06 14:41:18 -0800

    v0.7_working

---
+++ src/Person.java
@@ -0,0 +1,39 @@
+/* This is a basic Person class.  There is one field, a private String 'name'
+*/
+public class Person {
+	
+	private String name;
+	
+	public Person(String str){     // a constructor that sets the name 
+		name = str;
+	}
+	public Person(){            // a constructor if the name isn't known
+	 	name = null;	
+	}
+
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String str) {
+		name = str;
+	}
+	
+	public static void main(String[] args) {   // a main for testing
+		// TODO Auto-generated method stub
+		
+		
+		Person person = new Person("Barney Rubble");
+		Person person1 = new Person();
+		Tables tables = new Tables();
+		tables.setTables(tables.tabletest);
+		/*System.out.println(person.getName());
+		System.out.println(person1.getName());
+		person1.setName("Fred Flintstone");
+		System.out.println(person1.getName());
+	    */
+	}
+	
+
+}
+
---
+++ src/Tables.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class Tables {
+	
+	
+	static int totalGuests = myScanner1();
+	static int maxGuests = myScanner2();
+	private static Scanner sc1;
+	private static Scanner sc2;
+	static int indicator = totalGuests % maxGuests;
+	
+	public static int myScanner1(){
+		System.out.println("How many guests will be attending?");
+		sc1 = new Scanner(System.in);
+	    int totalGuests = sc1.nextInt();
+		return totalGuests;
+	}
+	public static int myScanner2() {
+		System.out.println("How many guests will each table hold?");
+		sc2 = new Scanner(System.in);
+		int maxGuests = sc2.nextInt();
+		return maxGuests;
+	}
+
+	static int fullTables = totalGuests/maxGuests;
+	
+	
+	static int[] tableLG = new int[2];
+	static int[] tableSM = new int[2];
+	
+	static int[] fullSet = new int[4];
+	
+	public void setTables (int[] array) {
+		fullSet[0] = array[0];
+		fullSet[1] = array[1];
+		fullSet[2] = array[2];
+		fullSet[3] = array[3];
+	}
+		
+	
+	public int[] getTables () {
+		fullSet [0] = tableLG[0];
+		fullSet[1] = tableLG[1];
+		fullSet[2] = tableSM[0];
+		fullSet[3] = tableSM[1];
+		return fullSet;
+
+	}
+	
+	public static void main(String[] args) {
+		if(indicator == 0){
+			int numPerfTables = maxGuests;
+			int perfTableSize = totalGuests/maxGuests;
+			
+			System.out.println("With " + totalGuests + " guests and a maximum of " 
+			+ maxGuests + " per table, the optimal seating arrangement should have " 
+			+ numPerfTables + " tables of " + perfTableSize);
+			tableLG[0] = numPerfTables;
+			tableLG[1] = maxGuests;
+		}
+		else {
+			int numSmallTables = maxGuests - indicator;
+			int smallTableSize = maxGuests - 1;
+			int numLargeTables = fullTables - numSmallTables + 1;
+			
+			
+			
+			
+		    System.out.println("With " + totalGuests + " guests and a maximum of "
+	        + maxGuests + " per table, the optimal seating arrangemnt should have "
+	        + numLargeTables + " tables of " + maxGuests + " , and " + numSmallTables +  " tables of "
+	        +  smallTableSize + " guests");
+		    //int p = (numLargeTables*maxGuests)+(numSmallTables*smallTableSize);
+		    //System.out.println(p);
+		    tableLG[0] = numLargeTables;
+		    tableSM[0] = numSmallTables;
+		    tableLG [1] = maxGuests;
+		    tableSM [1] = smallTableSize;
+		
+		
+		}
+		System.out.println(getTables());
+	}
+	
+}
